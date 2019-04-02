e26562fec76e8188d9a22f35759fc407cf025410
huixiong <huixionghexiyi@gmail.com>
2019-04-02 19:20:47 +0800

    Site updated: 2019-04-02 19:20:47

---
+++ test/test.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.util.Arrays;
+
+public class test {
+
+    public int partition(int nums[], int left, int right) {
+        int pivot = right;
+        System.out.println(nums[right]);
+        int index = left;
+        int t = 0;
+        for (int i = left; i < right; i++) {
+
+            if (nums[i] < nums[pivot]) {
+                t = nums[i];
+                nums[i] = nums[index];
+                nums[index] = t;
+                index++;// 自增
+            }
+            System.out.println(Arrays.toString(nums));
+        }
+        // swap(nums[index],nums[pivot]);
+        t = nums[index];
+        nums[index] = nums[pivot];
+        nums[pivot] = t;
+        return index;
+    }
+
+    void qSort(int nums[], int left, int right) {
+        if (left < right) {
+            int pivot = partition(nums, left, right);
+            qSort(nums, left, pivot - 1);
+            qSort(nums, pivot + 1, right);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] x = new int[] { 9, 4, 1, 6, 7, 2, 3, 5, 8 };
+        test t = new test();
+        t.qSort(x, 0, x.length - 1);
+
+    }
+}
